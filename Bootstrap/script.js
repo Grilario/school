cb14e9ee236c442fc6e9b54f90fa18088013fddc
Grilario <luisfernandinho094@gmail.com>
2021-03-08 11:05:36 -0300

    olokinho

---
+++ Bootstrap/script.js
@@ -0,0 +1,32 @@
+const menssage = document.getElementsByClassName('menssage')[0]
+
+function aninMenssage() {
+    menssage.style.bottom = "6vh"
+
+    setTimeout(() => {menssage.style.bottom = "-6vh"}, 1800)
+}
+
+function generateColors(){
+
+        
+    const divCores = document.querySelectorAll('.col-md')
+
+    for(i = 0; i < divCores.length; i++){
+        const cor = `#${(Math.random() * 0xFFFFFF<<0).toString(16)}`
+
+        divCores[i].firstElementChild.style.backgroundColor = cor
+        divCores[i].lastElementChild.textContent = cor
+    }
+}
+
+function copy() {
+    const element = event.currentTarget
+
+    navigator.clipboard.writeText(element.lastElementChild.textContent)
+
+    console.log(element.lastElementChild.textContent)
+
+    aninMenssage()
+}
+
+generateColors()
